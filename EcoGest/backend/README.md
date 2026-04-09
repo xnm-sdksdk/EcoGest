b34515e242d2d598b73d40511c778959fb194dde
xnm <n.mendonca@protonmail.com>
2026-04-09 19:02:40 +0100

    entry file, tsx, helmet, cors, rate limit config

---
+++ EcoGest/backend/README.md
@@ -0,0 +1,7 @@
+# EcoGest API
+
+## How to run the project
+
+The project uses tsx which is an TypeScript execution environment for Node, with this tool we can run the TS code in Node without needing to compile it first.
+
+`npx ts-node src/main.ts`
--- EcoGest/backend/src/main.ts
+++ EcoGest/backend/src/main.ts
@@ -1,2 +1,20 @@
-const port = 8080;
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
+const PORT = 8080;
+const app = express();
+app.use(helmet());
+app.use(cors());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+app.use(express.json());
+app.use(limiter);
+
+app.listen(PORT, () => console.log(`EcoGest API running on port ${PORT}`));
