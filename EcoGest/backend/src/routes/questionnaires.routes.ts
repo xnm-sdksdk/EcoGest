42c61eac1f82dd275105bf44ed484bb93a5149d8
tiagojpt <40220427@esmad.ipp.pt>
2026-05-23 00:07:31 +0100

    Questionnaires swagger and postman collections

--- EcoGest/backend/src/routes/questionnaires.routes.ts
+++ EcoGest/backend/src/routes/questionnaires.routes.ts
@@ -5,34 +5,54 @@ const router = Router();
 
 const questionnaireController = new QuestionnaireController();
 
-router.get(
-  "/projects/:id/questionnaires",
-  questionnaireController.getProjectQuestionnaires,
-);
+router.get("/projects/:id/questionnaires", (req, res, next) => {
+  /* #swagger.tags = ['Questionnaires']
+     #swagger.summary = 'Listar questionários de um projeto'
+  */
+  next();
+}, questionnaireController.getProjectQuestionnaires);
 
-router.post(
-  "/projects/:id/questionnaires",
-  questionnaireController.createQuestionnaire,
-);
+router.post("/projects/:id/questionnaires", (req, res, next) => {
+  /* #swagger.tags = ['Questionnaires']
+     #swagger.summary = 'Criar questionário num projeto'
+     #swagger.parameters['obj'] = {
+       in: 'body',
+       schema: { $ref: '#/definitions/CreateQuestionnaireRequest' }
+     }
+  */
+  next();
+}, questionnaireController.createQuestionnaire);
 
-router.get(
-  "/questionnaires/:id",
-  questionnaireController.getQuestionnaireById,
-);
+router.get("/questionnaires/:id", (req, res, next) => {
+  /* #swagger.tags = ['Questionnaires']
+     #swagger.summary = 'Obter questionário por ID'
+  */
+  next();
+}, questionnaireController.getQuestionnaireById);
 
-router.put(
-  "/questionnaires/:id",
-  questionnaireController.updateQuestionnaireById,
-);
+router.put("/questionnaires/:id", (req, res, next) => {
+  /* #swagger.tags = ['Questionnaires']
+     #swagger.summary = 'Atualizar questionário'
+     #swagger.parameters['obj'] = {
+       in: 'body',
+       schema: { $ref: '#/definitions/UpdateQuestionnaireRequest' }
+     }
+  */
+  next();
+}, questionnaireController.updateQuestionnaireById);
 
-router.delete(
-  "/questionnaires/:id",
-  questionnaireController.deleteQuestionnaireById,
-);
+router.delete("/questionnaires/:id", (req, res, next) => {
+  /* #swagger.tags = ['Questionnaires']
+     #swagger.summary = 'Eliminar questionário'
+  */
+  next();
+}, questionnaireController.deleteQuestionnaireById);
 
-router.put(
-  "/questionnaires/:id/publish",
-  questionnaireController.publishQuestionnaireById,
-);
+router.put("/questionnaires/:id/publish", (req, res, next) => {
+  /* #swagger.tags = ['Questionnaires']
+     #swagger.summary = 'Publicar questionário'
+  */
+  next();
+}, questionnaireController.publishQuestionnaireById);
 
 export default router;
